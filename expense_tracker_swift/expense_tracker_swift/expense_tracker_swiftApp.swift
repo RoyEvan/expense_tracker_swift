7593a7c9b0d92a4195b24cbd501eabd6eea5c7d1
Nana <najwa.almira2005@gmail.com>
2024-09-17 11:28:07 +0700

    Add Xcode project files

---
+++ expense_tracker_swift/expense_tracker_swift/expense_tracker_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_tracker_swiftApp.swift
+//  expense_tracker_swift
+//
+//  Created by MacBook Air on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct expense_tracker_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
