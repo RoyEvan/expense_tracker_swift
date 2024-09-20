74a7fd6e16e38a894a1101d8bbfa559de9f0eb80
jesica patricia <jesicapatricia2003@gmail.com>
2024-09-20 10:58:33 +0700

    savings

---
+++ ExpenseTracker/Model/SavingsModel.swift
@@ -0,0 +1,8 @@
+//
+//  SavingsModel.swift
+//  ExpenseTracker
+//
+//  Created by MacBook Air on 20/09/24.
+//
+
+import Foundation
---
+++ ExpenseTracker/View/SavingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SavingsView.swift
+//  ExpenseTracker
+//
+//  Created by MacBook Air on 20/09/24.
+//
+
+import SwiftUI
+
+struct SavingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavingsView()
+}
